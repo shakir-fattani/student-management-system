a361bef6ccc6643bcff001403db0786f5c8bc2cf
Shakir Fattani <m.shakirfattani@gmail.com>
2025-05-07 12:03:39 +0400

    problem 2 is also resolved, for adding

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -2,28 +2,34 @@ const asyncHandler = require("express-async-handler");
 const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { body } = req;
+    const students = await getAllStudents(body);
+    res.json({ students });
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { body } = req;
+    const { message } = await addNewStudent(body);
+    res.json({ message });
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { body } = req;
+    const { message } = await updateStudent(body);
+    res.json({ message });
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { id } = req.params;
+    const student = await getStudentDetail(id);
+    res.json(student);
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
-
+    const { id } = req.params;
+    const { status } = req.body;
+    const { message } = await setStudentStatus({ userId: id, status, reviewerId: null });
+    res.json({ message });
 });
 
 module.exports = {
